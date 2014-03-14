e8eec0204bdbe1e0b7fbb2d002b76c5078ca8c3c
boywhoroared <developer@theboywhoroared.com>
2014-03-14 00:14:34 -0400

    add a simple install script.
    use `ln -sFi` to symlink configs. -i prompts to overwrite an existing file.

---
+++ install.sh
@@ -0,0 +1,13 @@
+# basic install 
+ln -sFi ~/.dotfiles/etc ~/etc 
+ln -sFi ~/.dotfiles/aliases ~/.aliases
+ln -sFi ~/.dotfiles/env ~/.env
+ln -sFi ~/.dotfiles/bash_prompt ~/.bash_prompt
+ln -sFi ~/.dotfiles/bashrc ~/.bashrc
+ln -sFi ~/.dotfiles/bash_profile ~/.bash_profile
+ln -sFi ~/.dotfiles/vim ~/.vim
+ln -sFi ~/.dotfiles/vimrc ~/.vimrc
+ln -sFi ~/.dotfiles/vimrc.bundles ~/.vimrc.bundles
+ln -sFi ~/.dotfiles/gvimrc ~/.gvimrc
+ln -sFi ~/.dotfiles/gitconfig ~/.gitconfig
+ln -sFi ~/.dotfiles/tmux.conf ~/.tmux.conf
