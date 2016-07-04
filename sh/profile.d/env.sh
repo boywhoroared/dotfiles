6cfc5bc70bf9d419740d518905d8f4dcddfde91b
Mario <developer@theboywhoroared.com>
2016-07-04 11:19:28 -0400

    vim: plugin updates; fix bad gvimrc

--- sh/profile.d/env.sh
+++ sh/profile.d/env.sh
@@ -1,6 +1,6 @@
 export VISUAL="mvim"
 export EDITOR="vim"
 export TERM="xterm-256color"
-export LC_CTYPE="en_GB.UTF-8"
-export LC_ALL="en_GB.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
 export CLICOLOR="yes"
