74ccd35cd19c2e55a1ba29a42af314689e8a7775
= <developer@theboywhoroared.com>
2016-10-13 18:36:01 -0400

    remove go-lang from path

--- sh/profile.d/path.sh
+++ sh/profile.d/path.sh
@@ -4,14 +4,13 @@ PATH="/usr/local/sbin:$PATH"
 # Node
 PATH="$HOME/.node/bin:$PATH"
 
-# Go
-export GOPATH=$HOME/.golang
-export GOROOT=/usr/local/opt/go/libexec
-PATH=$PATH:$GOPATH/bin:$GOROOT/bin
-
 # Python PIP
 # Install packages using: `pip install --user PACKAGE`
 PATH="$PATH:$HOME/Library/Python/2.7/bin"
+
+# Ruby Gems
+# Install gems using: `gem install --user-install GEM`
+PATH="$PATH:$HOME/.gem/ruby/2.0.0/bin"
 
 # Local executables
 PATH="$HOME/.local/bin:$HOME/.bin:$PATH"
