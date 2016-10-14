65742b4104ee53404cb54fb906ba35ddcdd5efb6
Mario <developer@theboywhoroared.com>
2016-10-13 23:23:53 -0400

    Fix symlinks for installing dotfiles.

--- install.sh
+++ install.sh
@@ -3,13 +3,16 @@ ln -sFi ~/.dotfiles/bin ~/.local/bin
 ln -sFi ~/.dotfiles/etc ~/.etc
 ln -sFi ~/.dotfiles/aliases ~/.aliases
 ln -sFi ~/.dotfiles/env ~/.env
-ln -sFi ~/.dotfiles/bash_prompt ~/.bash_prompt
-ln -sFi ~/.dotfiles/bashrc ~/.bashrc
-ln -sFi ~/.dotfiles/bash_profile ~/.bash_profile
+
+ln -sFi ~/.dotfiles/bash/bash_prompt ~/.bash_prompt
+ln -sFi ~/.dotfiles/bash/bashrc ~/.bashrc
+ln -sFi ~/.dotfiles/bash/bash_profile ~/.bash_profile
+
 ln -sFi ~/.dotfiles/vim ~/.vim
-ln -sFi ~/.dotfiles/vimrc ~/.vimrc
 ln -sFi ~/.dotfiles/vimrc.bundles ~/.vimrc.bundles
-ln -sFi ~/.dotfiles/gvimrc ~/.gvimrc
-ln -sFi ~/.dotfiles/gitconfig ~/.gitconfig
-ln -sFi ~/.dotfiles/gitmessage ~/.gitmessage
+ln -sFi ~/.dotfiles/gvimrc ~/vim/.gvimrc
+
+ln -sFi ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -sFi ~/.dotfiles/git/gitmessage ~/.gitmessage
+
 ln -sFi ~/.dotfiles/tmux.conf ~/.tmux.conf
