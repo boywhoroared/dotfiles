943c70663db40523866239a079a4e4a06b772314
Mario <developer@theboywhoroared.com>
2018-12-22 21:43:06 -0400

    Move `scripts` to local `bin`
    Because `scripts` has to be on the path anyway.

---
+++ local/.local/bin/macos-dock-add-spacer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}' 
+killall Dock
--- scripts/macos-dock-reset.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Remove the dock configuration and forcibly kill the Dock
-defaults delete com.apple.dock; killall Dock
